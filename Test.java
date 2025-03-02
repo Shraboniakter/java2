712e9eead8adac46edc324bd36399fc75fac85ee
shrabon <shrabonbhuiyan708@gmail.com>
2025-03-02 15:23:26 +0600

    3rd commit

--- Test.java
+++ Test.java
@@ -1,6 +1,17 @@
+class A{
+    String name;
+    int id;
+    A(){
+        System.out.println("This is Constructor");
+    }
+     void display(){
+        System.out.println("This is display function");
+     }
+}
+
 public class Test{
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Done");
+        A obj =  new A();
+        obj.display();
     }
 }
